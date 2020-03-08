5a9de0c5639249f79fd4410ca1dc233f087de67e
najm09 <fnajm09@gmail.com>
2020-03-08 22:08:33 +0530

    printing linkedList using Insert function

---
+++ LinkedList/iterative LinkedList/createListByinsert.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node 
+{
+	int data;
+	struct Node * next;
+}*head=NULL,*last=NULL;
+
+struct Node * createNode(int value)
+{
+	struct Node * node = (struct Node *)malloc(sizeof(struct Node));
+	node->data = value;
+	node->next = NULL;
+	return node;
+}
+
+void createLinkList(int value)
+{
+	struct Node * node;
+	node = createNode(value);
+	if(head == NULL)
+	{
+		head = last = node ;    // all the nodes are at one since there is no other node
+	}
+	else
+	{
+		last->next = node ;  // current node becomes the next to the previous last node
+		last = node; 		//here the current node "node" is updated as last node "last"
+	}
+}
+
+void ShowList(int nodes)
+{
+	for( int i = 0; i < nodes ; i++)
+	{
+		printf("%d ", head->data );
+		head = head->next;
+	}
+}
+
+int main()
+{
+	int value,nodes;
+	printf("Enter number of nodes");
+	scanf("%d", &nodes);
+	printf("\nEnter all elements of the link list:\n");
+	for(int i = 0; i < nodes; i++){
+		scanf("%d", &value);
+		createLinkList(value);
+	}
+	ShowList(nodes);
+	return 0;
+}
