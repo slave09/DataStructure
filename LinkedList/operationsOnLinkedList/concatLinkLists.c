0a3e0977c93e6d8783fcac6cc45f05ea4a45bdb6
najm09 <fnajm09@gmail.com>
2020-03-10 02:25:39 +0530

    concatenation of two linkLists

---
+++ LinkedList/operationsOnLinkedList/concatLinkLists.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node 
+{
+  int data;
+  struct Node * next;
+}*first = NULL , *second = NULL , *third = NULL , *lats = NULL;
+
+struct Node * createNode(int value)
+{
+  struct Node * node = (struct Node *)malloc(sizeof(struct Node));
+  node->data = value;
+  node->next = NULL;
+  return node;
+}
+
+struct Node * createLinkList(int values[], int length)
+{
+ first= createNode(values[0]);
+ struct Node *current ,*last = first;
+ for(int i = 1; i < length; i++){
+  current = createNode(values[i]);
+  last->next = current;
+  last = current;
+ }
+}
+
+struct Node * createSecondList(int values[], int length)
+{
+ second = createNode(values[0]);
+ struct Node *current ,*last = second;
+ for(int i = 1; i < length; i++){
+  current = createNode(values[i]);
+  last->next = current;
+  last = current;
+ }
+}
+
+struct Node * joinTwoList(struct Node * head , struct Node * second)
+{
+  third = first;
+  while(first->next != NULL)
+  {
+    first = first->next;
+  }
+
+  first->next = second;
+  return third;
+}
+
+void ShowList(struct Node * head)
+{
+  while(head != NULL)
+  {
+    printf("%d ", head->data);
+    head = head->next;
+  }
+}
+
+int main()
+{
+  int values1[] = {2, 4, 6, 8, 10};
+  int values2[]= {1,3,5};
+  createLinkList(values1, 5);
+  createSecondList(values2, 3);
+  ShowList(first);
+  printf("\n");
+  ShowList(second);
+  printf("\n");
+  joinTwoList(first, second);
+  ShowList(third);
+}
