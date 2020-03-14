36fa0706de7039fa3792d0b2b2e62e3efa2a7b4e
najm09 <fnajm09@gmail.com>
2020-03-15 02:24:14 +0530

    displaying circular LinkedList iteratively

---
+++ LinkedList/CircularLinkList/iterativeDisplay.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node{
+  int data;
+  struct Node * next;
+}*Head;
+
+struct Node * createCircularNode(int value){
+  struct Node * node = (struct Node *)malloc(sizeof(struct Node));
+  node->data = value;
+  node->next = node;
+  return node;
+}
+
+void createLinkList(int values[], int length){
+  struct Node *last, *current;
+  Head = createCircularNode(values[0]);
+  last = Head;
+  for (int i = 1; i < length; ++i)
+  {
+    current = createCircularNode(values[i]);
+    last->next = current;
+    current->next = Head;
+    last = current;
+  }
+}
+
+void PrintList(struct Node * head){
+  head = Head;
+  do{
+    printf("%d ", head->data);
+    head = head->next;
+  }while(head != Head);
+  printf("\n");
+}
+
+int main(){
+  int values[] = {1, 2, 3, 4, 5};
+  createLinkList(values, 5);
+  PrintList(Head);
+  return 0;
+}
