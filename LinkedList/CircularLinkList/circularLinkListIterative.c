c2c732ef9499ee42a0625ad3841d97d5d658b571
najm09 <fnajm09@gmail.com>
2020-03-17 01:12:14 +0530

    insert operation on circular Linkedlist

--- LinkedList/CircularLinkList/circularLinkListIterative.c
+++ LinkedList/CircularLinkList/circularLinkListIterative.c
@@ -26,6 +26,15 @@ void createLinkList(int values[], int length){
   }
 }
 
+int calculateLength(struct Node * head){
+  int length = 0 ;
+  do{
+    length++;
+    head = head->next;
+  }while(head != Head);
+  return length;
+}
+
 void PrintList(struct Node * head){
   head = Head;
   do{
@@ -35,9 +44,42 @@ void PrintList(struct Node * head){
   printf("\n");
 }
 
+/**
+*inserts a new node in the linkList
+@param pointer to the Head of the circular LinkList
+@param index at which new node to be inserted
+@param value to be inserted inside the new node
+*/
+
+void insertValue(struct Node * head, int index, int value){
+  struct Node * node = createCircularNode(value);
+  if(index < 0 || index > calculateLength(Head))
+    return;
+  if(index == 0){
+    if(head == NULL){
+      head = node;
+    }
+    else{
+      while(head->next != Head)
+        head = head->next;
+      node->next = Head;
+      head->next = node;
+      Head = node;
+    }
+  }
+  else{
+    for (int i = 0; i < index-1; i++)
+      head = head->next;
+    node->next = head->next;
+    head->next = node;
+  }
+}
+
 int main(){
   int values[] = {1, 2, 3, 4, 5};
   createLinkList(values, 5);
+  PrintList(Head);
+  insertValue(Head, 0, 6);
   PrintList(Head);
   return 0;
 }
