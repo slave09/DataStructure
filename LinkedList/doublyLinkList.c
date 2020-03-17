6fa5054eb87c8b23677c0340f62136b88f4cf1f6
najm09 <fnajm09@gmail.com>
2020-03-17 15:57:57 +0530

    display operation on doubly LinkedList

--- LinkedList/doublyLinkList.c
+++ LinkedList/doublyLinkList.c
@@ -1,6 +1,8 @@
 /*
   Operations on doubly LinkList
-  1. Display
+  1. Create
+  2. Display
+  3. calculateLength
 */
 
 
@@ -14,6 +16,47 @@ struct Node
   struct Node * next;
 }*Head;
 
+struct Node * CreateNode(int value){
+  struct Node * node = (struct Node *)malloc(sizeof(struct Node));
+  node->prev = NULL;
+  node->data = value;
+  node->next = NULL;
+  return node;
+}
+
+void CreateLinkList(int values[], int length){
+  Head = CreateNode(values[0]);
+  struct Node *current, *last = Head;
+  for (int i = 1; i < length; ++i)
+  {
+    current = CreateNode(values[i]);
+    last->next = current;    
+    current->prev = last;
+    last = current;
+  }
+}
+
+int calculateLength(struct Node * Head){
+  int length = 0;
+  while(Head){
+    length ++;
+    Head = Head->next;
+  }
+  return length;
+}
+void PrintList(struct Node * Head)
+{
+  while(Head != NULL){
+    printf("%d ", Head->data );
+    Head = Head->next;
+  }
+  printf("\n");
+}
+
 int main(){
+  int values[] = {1, 2, 3, 4, 5};
+  CreateLinkList(values, 5);
+  PrintList(Head);
+  printf("length:%d\n", calculateLength(Head) );
   return 0;
 }
