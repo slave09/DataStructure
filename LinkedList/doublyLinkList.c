53f2b0e4f9e9ecebb91a12787c8e66236e41d1ed
najm09 <fnajm09@gmail.com>
2020-03-17 13:16:35 +0530

    doubly LinkList

---
+++ LinkedList/doublyLinkList.c
@@ -0,0 +1,19 @@
+/*
+  Operations on doubly LinkList
+  1. Display
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+  struct Node * prev;
+  int data;
+  struct Node * next;
+}*Head;
+
+int main(){
+  return 0;
+}
