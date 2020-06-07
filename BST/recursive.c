6a79cab2c61cd57ddfe1c5905fb90d90bf94d3ef
najm09 <fnajm09@gmail.com>
2020-06-07 16:12:10 +0530

    deleting key from binary search tree

--- BST/recursive.c
+++ BST/recursive.c
@@ -5,6 +5,10 @@
   Operations: 
             1. Insert
             2. search
+            3. height
+            4. Predecessor
+            5. Successor
+            6. delete
 */
 
 #include<stdio.h>
@@ -14,7 +18,7 @@ struct Node {
   struct Node *left;
   struct Node *right;
   int data;
-}*root = NULL;
+};
 
 struct Node * insert(struct Node *node, int value){
   struct Node *temp;
@@ -54,12 +58,64 @@ struct Node * search(struct Node *root, int key){
   return NULL;
 }
 
+int height(struct Node *root){
+  int x, y;
+  if(!root) 
+    return 0;
+  x = height(root->left);
+  y = height(root->right);
+  return x > y ? x+1 : y+1;
+}
+
+struct Node* Predecessor(struct Node *root){
+  while(root && root->right != NULL){
+    root = root->right;
+  }
+  return root;
+}
+
+struct Node* successor(struct Node *root){
+  while(root && root->left != NULL)
+    root = root->left;
+  return root;
+}
+
+struct Node *delete(struct Node *node, int key){
+  struct Node *temp;
+  if(node == NULL) return NULL;
+  if(! root->left && ! root->right){
+    if(node == root)
+      root = NULL;
+    free(node);
+    return NULL;
+  }
+  if(root->data > key)
+  return delete(root->left, key);
+  else if(root->data < key)
+  return delete(root->right, key);
+  else{
+    if(height(root->left) > height(root->right)){
+      temp = Predecessor(root->left);
+      root->data = temp->data;
+      root->left = delete(root->left, temp->data);
+    }
+    else{
+      temp = successor(root->right);
+      root-data = temp->data;
+      root->right = delete(root->right, temp->data);
+    }
+  }
+  return node;
+}
+
 int main(){
+  struct Node *root = NULL;
   struct Node * result;
   root = insert(root,10);
   insert(root,5);
   insert(root,20);
   insert(root,15);
+  //delete(root, 15);
   inorder(root);
   result = search(root, 10); 
   if(result)
