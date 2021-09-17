f6ee29ee5d9eee25851a34d8a34187f5c88a3b58
najm09 <fnajm09@gmail.com>
2021-09-18 00:40:22 +0530

    Double hashing : code refactor

--- HashTable/doubleHashing.cpp
+++ HashTable/doubleHashing.cpp
@@ -2,17 +2,16 @@
  * Author : Fakhra Najm 
  * Email  : fnajm09@gmail.com
  * Topic  : Hash Table Implementation : "Double Hashing"
- * */
+ */
 
 #include<iostream>
 using namespace std;
 
 class HashMap{
-
 private:
 	int size;
-	int doubleKey ;
-	int * Array;
+	int doubleKey ; // First prime number from last of the array
+	int *Array;
 
 public:
 	HashMap(){}
@@ -27,41 +26,28 @@ public:
 };
 
 bool isPrime(int val){
-
 	if (val <= 1) return false;
-
 	for (int factor = 2; factor < val; factor++)
 		if (val % factor == 0) return false;
-
 	return true;
 }
 
 void HashMap :: create(){
 	int size, doubleKey;
-
 	cout << "Enter number of data items: ";
 	cin >> size;
-
 	this -> size = 2 * size ;
-
 	cout << "size of Hash Table is " << this -> size << endl;
-
 	this -> Array = new int[this -> size]; 
-
 	for(int key = 0; key < this -> size; ++key)
 		Array[key] = 0;
-
 	for(int key = this -> size; key >= 0; --key){
 		if(isPrime(key)){
 			this -> doubleKey = key;
 			break;
 		}
 	}
-
-	// cout << "DEBUG : doublekey : " <<  this -> doubleKey << endl;
-
 	cout << "Enter " << size << " data elements : " ;
-
 	for(int element = 0; element < size; ++element){
 		int val ;
 		cin >> val;
@@ -80,29 +66,27 @@ int HashMap :: first_hash(int val){
 }
 
 int HashMap :: second_hash(int val){
-	return doubleKey - (val % doubleKey);
+	return doubleKey - (val % doubleKey); // gives random index
 }
-
+/*
+	* Double hashing = h'(x) = (firstHasing + f(i)*secondHashing) % 10
+	* f(i) = i
+	* i = 0,1,2,3....
+*/
 int HashMap :: doubleHashing(int val){
-
 	int first_hash_key = first_hash(val);
 	int second_hash_key = second_hash(val);
 	int index = first_hash_key;
 	int i = 0;
-
 	while(this -> Array[index] != 0)
 		index = (first_hash_key + (i++) * (second_hash_key)) % size;
-	
 	return index;
 }
 
 void HashMap :: update(int val){
-
 	int hash_key = first_hash(val);
-
 	if(this -> Array[hash_key] == 0)
 		this -> Array[hash_key] = val;
-
 	else{
 		hash_key = doubleHashing(val);
 		this -> Array[hash_key] = val;
@@ -115,7 +99,6 @@ int HashMap :: search(int val){
 	else{
 		int second_hash_key = second_hash(val);
 		int i = 0;
-
 		while(Array[key] != val){
 			if(Array[key] == 0) return -1;
 			key = (first_hash(val) + (i++) * (second_hash_key)) % size;
@@ -127,17 +110,12 @@ int HashMap :: search(int val){
 int main(){
 	int val, find;
 	HashMap map;
-
 	map.create();
 	map.read();
-
 	cout << "Enter the value to be found:" << endl;
 	cin >> val;
-
 	find = map.search(val);
-
 	if(find == -1) cout << val << " NOT FOUND!" << endl;
 	else cout << val << " IS FOUNDED AT: " << find << endl;
-
  	return 0;
 }
